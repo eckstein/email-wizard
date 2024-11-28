7afdd4388f9fdc71ad8f009d2ea254a1f4d74974
Eckstein <zac@zaceckstein.com>
2024-11-28 09:22:50 -0800

    Fix index file structures

--- assets/js/components/index.js
+++ assets/js/components/index.js
@@ -1,26 +1,35 @@
+// Template Management
 export {
 	create_single_template,
 	duplicate_single_template,
+	saveTemplateFormData,
+	restore_templates,
+	delete_templates_forever,
+	templateTableAPI
+} from './editor';
+
+// Folder Operations
+export {
 	select_folder,
 	open_folder_title_editor,
 	create_new_wizard_folder,
 	rename_single_folder,
-	init_file_explorer,
+	init_file_explorer
+} from './fileExplorer';
+
+// Item Operations
+export {
 	move_items,
 	delete_items,
 	show_delete_confirm,
-	show_restore_confirm,
-	templateTableAPI
-} from "./fileExplorer";
+	show_restore_confirm
+} from './fileExplorer';
 
-export { restore_templates, delete_templates_forever } from "./fileExplorer/templates";
-
-export {saveTemplateFormData} from "./editor";
-
+// Team Management
 export {
 	init_team_handler,
 	handleNewTeam,
 	show_create_team_dialog,
 	createTeamRequest,
 	switch_team
-} from "./teams/teams-actions";
+} from './teams';
--- assets/js/utils/index.js
+++ assets/js/utils/index.js
@@ -1,16 +1,32 @@
-export { toggle_wizard_dropdown, hide_wizard_dropdown } from "./dropdown";
-export { init_slideover } from "./slideover";
-export { add_repeater_row, remove_repeater_row } from "./forms";
-export {
-	addEventListenerIfExists,
-	highlight_element,
-	handleFetchError,
-	handleHTTPResponse,
-	show_success_toast,
-	show_error_toast,
-	highlight_and_remove,
-	remove_url_parameter,
-} from "./functions";
-export { wiz_toast, checkUrlParamsForToasts } from "./swal2";
-export { switch_wizard_tab } from "./tabs";
-export { init_editable_folder_titles } from "./tinymce";
+// UI Related Utilities
+export { toggle_wizard_dropdown, hide_wizard_dropdown } from './dropdown';
+export { switch_wizard_tab } from './tabs';
+export { init_slideover } from './slideover';
+
+// Form & Input Handling
+export { dd_repeater_row, remove_repeater_row } from './forms';
+
+// DOM Manipulation
+export { 
+    addEventListenerIfExists,
+    highlight_element,
+    highlight_and_remove 
+} from './functions';
+
+// Notification & Feedback
+export { 
+    wiz_toast,
+    show_success_toast,
+    show_error_toast,
+    checkUrlParamsForToasts 
+} from './swal2';
+
+// Editor Configuration
+export { init_editable_folder_titles } from './tinymce';
+
+// HTTP & Response Handling
+export { 
+    handleFetchError,
+    handleHTTPResponse,
+    remove_url_parameter 
+} from './functions';
